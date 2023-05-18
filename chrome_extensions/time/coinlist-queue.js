d6f9570b66928cb6a499f212152ec9db90220f67
izag <izag@splinex.com>
2023-05-18 22:47:29 +0500

    Add tracker for coin list.

--- chrome_extensions/time/coinlist-queue.js
+++ chrome_extensions/time/coinlist-queue.js
@@ -8,7 +8,7 @@ fetch('https://api.db-ip.com/v2/free/self', { mode: 'cors' })
     if (data == null)
         return;
 
-    var ip = { my_ip : data.ipAddress };
+    const ip = {my_ip: data.ipAddress};
     chrome.storage.sync.set(ip, () => { console.log(ip); });
 });
 
@@ -19,30 +19,19 @@ async function postData(url = '', data = {}) {
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
-        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'same-origin', // include, *same-origin, omit
-        // headers: {
-        //     "Accept": "application/json",
-        //     "Content-type": "application/json"
-        // },
-        // redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *client
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    
+
     return response;
 }
 
 
 setInterval(() => {
-    const tm = document.getElementById('ct');
-    if (tm == null)
+    const val = document.getElementById('MainPart_lbUsersInLineAheadOfYou');
+    if (val == null)
         return;
 
-    const num = tm.innerHTML;
-    console.log("Time: " + num);
-    const datetime = new Date('1970-01-01T' + num + 'Z');
-
+    const num = val.innerHTML;
     chrome.storage.sync.get("my_ip", (item) => {
         if (item.my_ip == null)
             return;
@@ -52,7 +41,7 @@ setInterval(() => {
 
         postData('http://localhost:8888/browsers', {
             ip : item.my_ip,
-            time : datetime
+            position : num
         })
         .then((data) => {
             console.log(data);
